a6c7e957e47fde4e1012cc1c21fe0c4ac718d439
Fran <frablamo@gmail.com>
2016-10-13 20:47:03 +0200

    Initial commit

---
+++ src/main/java/com/codersVlc/RecruitingEasyApplication.java
@@ -0,0 +1,12 @@
+package com.codersVlc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitingEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitingEasyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codersVlc/RecruitingEasyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codersVlc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecruitingEasyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
